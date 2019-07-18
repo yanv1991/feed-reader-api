35666821d3cc5ab6df23ff6ea08dd42f7d132431
Yaser Nicaragua <yanv1991@gmail.com>
2019-07-18 17:33:41 -0600

    refactor saveFeed

--- controller.js
+++ controller.js
@@ -3,47 +3,54 @@ const parser = new Parser();
 
 const FeedSchema = require("./model");
 
-exports.saveFeed = async function({ body: { url }}, res, next) {
+exports.saveFeed = async function({ body: { url } }, res, next) {
   try {
     const feed = await parser.parseURL(url);
-  } catch (error) {
-    return next(error)
-  }
-  
-  const feedSchema = new FeedSchema({ url: url });
 
-  feedSchema.save(function(err, payload) {
-    if (err) {
-      throw err;
-    }
+    const feedSchema = new FeedSchema({ url: url });
 
-    return parser.parseURL(payload.url).then(response => {
+    feedSchema.save(function(err, payload) {
+      if (err) {
+        return next(err);
+      }
+
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Methods", "GET");
-      res.json(response);
-    }).catch((err) => console.error("Error", err))
-  });
+      res.json(feed);
+    });
+  } catch (error) {
+    res.status(500)
+    return next(error);
+  }
 };
 
 exports.getFeeds = function(req, res) {
-    const skip = req.query.skip || 0
-    const limit = req.query.limit || 0
-  FeedSchema.find().skip(Number(skip)).limit(Number(limit)).exec(function(err, payload) {
-    if (err) return res.send(err);
+  const skip = req.query.skip || 0;
+  const limit = req.query.limit || 0;
+  FeedSchema.find()
+    .skip(Number(skip))
+    .limit(Number(limit))
+    .exec(function(err, payload) {
+      if (err) return res.send(err);
 
-    const feedRequests = payload.map(feed => {
-      return parser.parseURL(feed.url);
-    });
-
-    Promise.all(feedRequests)
-      .then(response => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Methods", "GET");
-        // return only the first 6 items
-        res.json(response.map((current) => ({ ...current, items: current.items.slice(0, 10) })));
-      })
-      .catch(err => {
-        console.error("Error", err);
+      const feedRequests = payload.map(feed => {
+        return parser.parseURL(feed.url);
       });
-  });
+
+      Promise.all(feedRequests)
+        .then(response => {
+          res.setHeader("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Methods", "GET");
+          // return only the first 6 items
+          res.json(
+            response.map(current => ({
+              ...current,
+              items: current.items.slice(0, 10)
+            }))
+          );
+        })
+        .catch(err => {
+          console.error("Error", err);
+        });
+    });
 };
