ebdd3a6e7da949e096c58699a9410becfcd5819c
Yaser Nicaragua <yanv1991@gmail.com>
2019-07-18 14:07:01 -0600

    fix deprecation warning

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ exports.init = () => {
     "mongodb+srv://dev:dev@cluster0-z3ysl.mongodb.net/test?retryWrites=true&w=majority";
   let mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-  mongoose.connect(mongoDB);
+  mongoose.connect(mongoDB, { useNewUrlParser: true });
   mongoose.Promise = global.Promise;
   let db = mongoose.connection;
   db.on("error", console.error.bind(console, "MongoDB connection error:"));
